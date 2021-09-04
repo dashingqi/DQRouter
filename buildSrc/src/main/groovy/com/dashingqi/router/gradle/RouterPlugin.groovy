fbf68b707d5b3e5ccb8e118f7e8368db8a02114b
zhangqi61 <zhangqi61@baidu.com>
2021-09-04 18:42:32 +0800

    [Gradle插件发布到本地]

---
+++ buildSrc/src/main/groovy/com/dashingqi/router/gradle/RouterPlugin.groovy
@@ -0,0 +1,23 @@
+package com.dashingqi.router.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * RouterPlugin
+ */
+class RouterPlugin implements Plugin<Project> {
+
+    // 实现apply方法 注入我们插件的逻辑
+    @Override
+    void apply(Project project) {
+        println("I am from RouterPlugin ,apply from ${project.name}")
+        project.getExtensions().create("router", RouterExtension)
+        project.afterEvaluate {
+            // 拿到Extension
+            RouterExtension extension = project["router"]
+
+            println "用户设置的wiki路径为 == ${extension.wikiDir}"
+        }
+    }
+}
---
+++ router-gradle-plugin/src/main/groovy/com/dashingqi/router/gradle/RouterExtension.groovy
@@ -0,0 +1,8 @@
+package com.dashingqi.router.gradle
+
+/**
+ * Router 扩展
+ */
+class RouterExtension {
+    String wikiDir
+}
