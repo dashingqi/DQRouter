4891ae565a3b760e78eaf4e783c4963c46adbaad
zhangqi61 <zhangqi61@baidu.com>
2021-09-15 07:40:14 +0800

    [自动清理构建的产物]

--- buildSrc/src/main/groovy/com/dashingqi/router/gradle/RouterPlugin.groovy
+++ buildSrc/src/main/groovy/com/dashingqi/router/gradle/RouterPlugin.groovy
@@ -14,9 +14,19 @@ class RouterPlugin implements Plugin<Project> {
 
         println("I am from RouterPlugin ,apply from ${project.name}")
 
+        // 1. 自动将路径参数传递到注解处理器中（无需用户手动）
         if (project.extensions.findByName("kapt") != null) {
             project.extensions.findByName("kapt").arguments {
                 arg("root_project_dir", project.rootProject.projectDir.absolutePath)
+            }
+        }
+
+        // 2. 实现旧的构建产物自动清理(指定某一个构建产物进行清理)
+        project.clean.doFirst {
+            File routerMappingDir =
+                    new File(project.rootProject.projectDir, "router_mapping")
+            if (routerMappingDir.exists()) {
+                routerMappingDir.deleteDir()
             }
         }
 
